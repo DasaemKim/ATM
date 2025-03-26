354031d0b90d261363aad7dccb7f970e0f6ad9c7
DasaemKim <kds000203@gmail.com>
2025-03-26 10:51:45 +0900

    <Feat>:UserData ID&Password 추가

--- My project/Assets/Scripts/UserData.cs
+++ My project/Assets/Scripts/UserData.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 [System.Serializable]
 public class UserData
 {
+    public string ID;
+    public string Password;
     public string Name = "±è´Ù»ù";
     public int Balance = 50000;
     public int Cash = 100000;
 
-    public UserData(string name, int balance, int cash)
+    public UserData(string id, string passward, string name, int balance, int cash)
     {
+        ID = id;
+        Password = passward;
         Name = name;
         Balance = balance;
         Cash = cash;
